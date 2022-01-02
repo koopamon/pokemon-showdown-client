d8d50dda5deaefc980fe116ec461fe81690f88c8
RyanDCB2 <79123792+RyanDCB2@users.noreply.github.com>
2022-01-01 23:39:20 -0500

    smarties

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (5c4d21f7/55fadf43)";
+Config.version = "0.11.2 (08f60cdc/55fadf43)";
 
 Config.routes = {
 	root: 'dragonheavenserver.herokuapp.com',

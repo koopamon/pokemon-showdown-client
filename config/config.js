ad94b0d0686ef088d7cbe1406ce5b16958e4bbe4
RyanDCB2 <79123792+RyanDCB2@users.noreply.github.com>
2022-01-02 00:14:20 -0500

    bran fails me

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (08f60cdc/55fadf43)";
+Config.version = "0.11.2 (d8d50dda/55fadf43)";
 
 Config.routes = {
 	root: 'dragonheavenserver.herokuapp.com',
